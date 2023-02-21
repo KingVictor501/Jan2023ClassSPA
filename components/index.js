8082771241eb90715a29b5552793b563113b0705
Doc Carson <kvcarsonjr@gmail.com>
2023-02-21 18:40:22 -0500

    Class Spa Updates

--- components/index.js
+++ components/index.js
@@ -2,14 +2,3 @@ export { default as Header } from "./Header";
 export { default as Nav } from "./Nav";
 export { default as Main } from "./Main";
 export { default as Footer } from "./Footer";
-
-import { Header, Nav, Main, Footer } from "./components";
-
-function render() {
-  document.querySelector("#root").innerHTML = `
-  ${Header()}
-  ${Nav()}
-  ${Main()}
-  ${Footer()}
-  `;
-}
