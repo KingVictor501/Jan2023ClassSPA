6561d76d8400da0f984c98de2ad7c0a7ca923c83
Doc Carson <kvcarsonjr@gmail.com>
2023-02-21 22:38:54 -0500

    SPA Updates

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { Header, Nav, Main, Footer } from "./components";
 import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const router = new Navigo("/");
 
@@ -15,23 +19,46 @@ function render(state = store.Home) {
   afterRender();
   router.updatePageLinks();
 }
+
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
 router.hooks({
   before: (done, params) => {
-    const page =
-      params && params.hasOwnProperty("page")
-        ? capitalize(params.page)
-        : "Home";
+    const view =
+      params && params.data && params.data.view
+        ? capitalize(params.data.view)
+        : "Home"; // Add a switch case statement to handle multiple routes
     // Add a switch case statement to handle multiple routes
-    switch (page) {
+    switch (view) {
       case "Home":
         axios
           .get(
             // Replace the key provided here with your own key from openweathermap
-            `https://api.openweathermap.org/data/2.5/weather?q=st%20louis&appid=5b86798a64d5b412d4d1900901150a41
-            `
+            `https://api.openweathermap.org/data/2.5/weather?q=st%20louis&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}`
           )
           .then((response) => {
             console.log(response.data);
+            const kelvinToFahrenheit = (kelvinTemp) =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            // Save Data into state
+            store.Home.weather = {};
+            store.Home.weather.city = response.data.name;
+            store.Home.weather.temp = kelvinToFahrenheit(
+              response.data.main.temp
+            );
+            store.Home.weather.feelsLike = kelvinToFahrenheit(
+              response.data.main.feels_like
+            );
+            store.Home.weather.description =
+              response.data.weather[0].description;
+            console.log(store.Home.weather);
+
             done();
           });
         break;
@@ -48,12 +75,6 @@ router.hooks({
     render(store[view]);
   },
 });
-function afterRender() {
-  // add menu toggle to bars icon in nav bar
-  document.querySelector(".fa-bars").addEventListener("click", () => {
-    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-  });
-}
 
 router
   .on({
